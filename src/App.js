4bce3825f5ab6b605159ed9c6264322082ee54ec
ElhamImdad <Elham.imdad@gmail.com>
2020-08-07 16:40:21 +0300

    add simple form using formik and yup library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './components/SignUp';
+import WithUseFormik from './components/WithUseFormik';
+import AxiosFormik from './components/AxiosFormik';
+// import Form from './components/WithRecomposer';
+import MultiStepsForm from './components/MultiStepsForm';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <SignUp/> */}
+      <MultiStepsForm/>
+      {/* <div>
+    <Form form={{phoneNumber: '+7', authCode: ''}} />
+  </div> */}
+      {/* <WithUseFormik/> */}
+      {/* <AxiosFormik/> */}
     </div>
   );
 }
